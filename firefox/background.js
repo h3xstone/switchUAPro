fda400f62b5c92d514412fbb210441d809876ede
Matt Glow <119514785+h3xstone@users.noreply.github.com>
2023-02-10 23:19:47 +0100

    init MV2

---
+++ firefox/background.js
@@ -0,0 +1,247 @@
+"use strict";
+
+var selOS;
+var selBRO;
+var status;
+var UA_list;
+var ua;
+var initw = null;
+var rnd_c = 0;
+var rnd_nr;
+var rnd_flag;
+var rnd_t = "requests";
+var last_type = null;
+var rndOsSel = null;
+
+fetch("listAgent.json")
+.then(response=> response.json())
+.then(json_data => {UA_list = json_data});
+
+function rewriteUAHeader(e) {
+	for (let hdr of e.requestHeaders) {
+		if (hdr.name.toLowerCase() === "user-agent") {
+			hdr.value = ua;
+		}
+	}
+	
+	if(rnd_flag && (rnd_t === "requests")) {
+		if(rnd_c < rnd_nr){
+			rnd_c += 1;	
+		} else {
+			loop();
+			rnd_c = 0;
+		}
+	}
+	return {requestHeaders: e.requestHeaders};
+}
+
+function setOS(par){
+	selOS = par;
+	updateList();
+}
+
+function setBrowser(par){
+	if(par === 'all'){
+		selBRO = null;
+	} else {
+		selBRO = par;
+	}
+	updateList();
+}
+
+function setSELUA(par){
+	browser.runtime.sendMessage({cmd: "showSel", sel: par});
+}
+
+function setUA(par){
+	ua = par;
+	localStorage.setItem("ualv",ua);
+}
+
+function resetALL(){
+	ua = navigator.userAgent;
+	localStorage.setItem("ualv",ua);
+	rnd_flag = false;
+	rnd_nr = null;
+	rnd_c = 0;
+	rndOsSel = null;
+	last_type = null;
+	
+	unsetTimer();
+	localStorage.removeItem("uars");
+}
+
+function updateList(){
+	if(!selOS){
+		return;
+	}			
+	let res = '<select name="slua" id="slua" size="10">';
+	let cont = 0;
+	if(selBRO){
+		const arr = UA_list[selOS][selBRO];
+		arr.forEach(function(el) {
+			cont++;
+			res = res.concat('<option class="optua">',el,'</option>');
+		});
+	} else {
+		const arr = UA_list[selOS];
+		for(const bro in arr){
+			arr[bro].forEach(function(el){
+				cont++;
+				res = res.concat('<option class="optua">',el,'</option>');
+			});
+		}
+	}
+	res = res.concat('</select>');
+	
+	browser.runtime.sendMessage({cmd: "buildList", list: res});
+}
+
+function getUA(){
+	if(!ua){
+		if(localStorage.getItem("ualv")) {
+			ua = localStorage.getItem("ualv");
+		} else {
+			ua = navigator.userAgent;
+		}
+	}
+	if(initw) {
+		return ua;
+	} else {
+		browser.runtime.sendMessage({cmd: "showUA", currUA: ua});	
+	}	
+}
+
+function clearQuery(){
+	selOS = null;
+	selBRO = null;
+}
+
+function setPower(v){
+	if(v){
+		status = "on";
+		browser.webRequest.onBeforeSendHeaders.addListener(rewriteUAHeader, {urls: ["<all_urls>"]}, ["blocking", "requestHeaders"]);
+
+		browser.browserAction.setBadgeText({text: 'ON'});
+		browser.browserAction.setBadgeBackgroundColor({color: '#008000'});
+		
+		if(rnd_flag && rnd_t==="minutes"){
+			setTimer(rnd_nr);
+		}
+	}else{
+		status = "off";
+		browser.webRequest.onBeforeSendHeaders.removeListener(rewriteUAHeader, {urls: ["<all_urls>"]}, ["blocking", "requestHeaders"]);
+		
+		browser.browserAction.setBadgeText({text: 'OFF'});
+		browser.browserAction.setBadgeBackgroundColor({color: '#FF6550'});
+		
+		if(rnd_flag && rnd_t==="minutes") {
+			unsetTimer();
+		}
+	}
+	localStorage.setItem("uaps",status);
+}
+
+function getPower(){
+	if(!status){
+		if (localStorage.getItem("uaps")) {
+			status = localStorage.getItem("uaps");
+		}
+	}
+	if (initw) {
+		if(status==="on"){
+			setPower(true);
+		} else {
+			setPower(false);
+		} 
+	} else {
+		browser.runtime.sendMessage({cmd: "power", pow: status});
+	}
+}
+
+window.addEventListener('load', function () {
+	initw = true;
+	getPower();
+	getUA();
+	getRandomUA();
+	initw = false;
+});
+
+function setRandomUA(s,n,t,o) {
+	rnd_flag = s;
+	rnd_nr = parseInt(n);
+	rnd_t = t;
+	rnd_c = 0;
+	rndOsSel = o;
+
+	localStorage.setItem("uars",JSON.stringify({"state":rnd_flag,"tot":rnd_nr,"type":rnd_t,"los":rndOsSel}));
+	
+	if(status === "off"){
+		return;
+	}
+
+	if(!last_type){
+		last_type = rnd_t;
+	}
+
+	if(rnd_t === "minutes") {
+		last_type = rnd_t;
+		if(rnd_flag) {
+			setTimer(rnd_nr);
+		} else {
+			unsetTimer();
+		}
+	} else if (rnd_t === "requests" && last_type === "minutes") {
+		unsetTimer();
+		last_type = rnd_t;
+	}
+}
+
+function getRandomUA() {
+	if (initw) {
+		if(!rnd_flag || !rnd_nr || !rnd_t || !rndOsSel) {
+			let rndSettings = localStorage.getItem("uars");
+			
+			if (rndSettings && rndSettings !== '{}'){
+				rndSettings = JSON.parse(rndSettings);
+				rnd_flag = rndSettings.state;
+				rnd_nr = rndSettings.tot;
+				rnd_t = rndSettings.type;
+				rndOsSel = rndSettings.los;
+
+				setRandomUA(rnd_flag,rnd_nr,rnd_t,rndOsSel);
+			}
+		}
+	} else {
+		browser.runtime.sendMessage({cmd: "random", state: rnd_flag, nrup: rnd_nr, type: rnd_t, showRndOs: rndOsSel});
+	}
+}
+
+function loop(){
+	let rndos;
+	if(rndOsSel) {
+		rndos = rndOsSel;
+	} else {
+		let klos = Object.keys(UA_list).length;
+		rndos = Object.keys(UA_list)[Math.floor(Math.random()*klos)];
+	}	
+	let klbr = Object.keys(UA_list[rndos]).length;
+	let rndbr = Object.values(UA_list[rndos])[Math.floor(Math.random()*klbr)];
+	let rndua = rndbr[Math.floor(Math.random()*rndbr.length)];
+	
+	setUA(rndua);
+}
+
+function setTimer(t) {
+	if(browser.alarms.onAlarm.hasListener(loop)){
+		browser.alarms.clearAll();
+	} else {
+		browser.alarms.onAlarm.addListener(loop);
+	}
+	browser.alarms.create("rndUAtimer", {periodInMinutes: t});
+}
+
+function unsetTimer() {
+	browser.alarms.clearAll();
+	browser.alarms.onAlarm.removeListener(loop);
+}
---
+++ firefox/popup/main.js
@@ -0,0 +1,93 @@
+var bgPage = browser.extension.getBackgroundPage();
+
+document.addEventListener("click", (e) => {
+	if (e.target.classList.contains("radioOS")) {
+		bgPage.setOS(e.target.value);
+	} else if (e.target.classList.contains("radioBRO")) {
+		bgPage.setBrowser(e.target.value);
+	} else if (e.target.classList.contains("optua")) {
+		bgPage.setSELUA(e.target.textContent);
+	} else if (e.target.classList.contains("sub")) {
+		const form = document.querySelector("form");
+		
+		if (e.target.id === 'apply') {
+			bgPage.setUA(form[0].value);
+
+			let os = document.querySelector("input[type='radio'][class='radioOS']:checked");
+			if(os) {
+				os = os.value;
+			}
+			bgPage.setRandomUA(form[3].checked, form[4].value, form[5].value, os);
+		} else if (e.target.id === 'reset') {
+			bgPage.resetALL();
+		}
+	} else if (e.target.id === 'togglePower') {
+		bgPage.setPower(e.target.checked);
+		bgPage.getPower();
+	} else if (e.target.id === 'toggleRnd') {
+		checkErr();
+	} else {
+		return;
+	}
+});
+
+browser.runtime.onMessage.addListener((msg) => {
+	if (msg.cmd === "buildList"){
+		document.getElementById("result").innerHTML = msg.list;	
+	} else if (msg.cmd === "showSel") {
+		document.getElementById("currUA").value = msg.sel;
+	} else if (msg.cmd === "showUA") {
+		document.getElementById("currUA").value = msg.currUA;
+	} else if(msg.cmd==="power"){
+		if(msg.pow==="on"){
+			document.getElementById("togglePower").checked = true;
+			document.getElementById("powerState").innerHTML = "ON";
+			document.getElementById("powerState").setAttribute("style","color: #0000FF");
+			browser.browserAction.setBadgeText({text: 'ON'});
+			browser.browserAction.setBadgeBackgroundColor({color: '#008000'});
+		}else{
+			document.getElementById("togglePower").checked = false;
+			document.getElementById("powerState").innerHTML = "OFF";
+			document.getElementById("powerState").setAttribute("style","color: #FF0000");
+			browser.browserAction.setBadgeText({text: 'OFF'});
+			browser.browserAction.setBadgeBackgroundColor({color: '#FF6550'});
+		}
+	} else if(msg.cmd === "random" ) {
+	 	document.getElementById("toggleRnd").checked = msg.state;
+	 	document.getElementById("rndNum").value = msg.nrup;
+	 	document.getElementById("rndType").value = msg.type;
+	 	
+	 	if(msg.state && msg.showRndOs){
+	 		document.querySelector("label[for='" + document.querySelector(`input[type='radio'][class='radioOS'][value="${msg.showRndOs}"`).id + "']").setAttribute("style","text-decoration: underline double #0000FF;");
+	 	}	
+	} else {
+		return;
+	}
+});
+
+document.addEventListener("readystatechange", (e) => {
+	bgPage.getUA();
+	bgPage.clearQuery();
+	bgPage.getPower();
+	bgPage.getRandomUA();
+});
+
+document.getElementById("rndNum").addEventListener("change", checkErr);
+
+function checkErr(){
+	let btn = document.getElementById("apply");
+	let nr = document.getElementById("rndNum");
+	let flag = document.getElementById("toggleRnd").checked;
+	
+	if(flag && (!nr.value || nr.value < 1 || nr.value > 999999)) {
+		btn.setAttribute("disabled",true);
+		nr.setAttribute("style","border: 2px solid #FF0000;");
+	} else {
+		btn.removeAttribute("disabled");
+		nr.removeAttribute("style");
+	}
+}
+
+document.getElementById("infoPop").addEventListener("click", (e)=>{
+	document.getElementById("txtPop").classList.toggle("show");
+});
